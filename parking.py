f0b2e3f6d0dc2f23ae63f5fdaf3b9a213fafbe5f
Nicholas Mandel <nwmandel@gmail.com>
2017-01-21 01:40:01 -0800

    template for parkinglot

---
+++ parking.py
@@ -0,0 +1,20 @@
+#Nick Mandel
+#hackucsc2017
+
+class lot:		#parking lot class
+
+	def__init__(self,taken)
+	TOTAL_SPOTS = 2000		#total east remote spots aproximation
+	self.num = 0			#number of open spots
+
+	def check_if_full(self):	#check if the lot is full
+		if num == TOTAL_SPOTS:
+			print("Parking lot is full. :(")
+		else:
+			print("Spots available :)")
+
+	def check_in(self):		#check into parking lot
+		num = num + 1
+
+	def leave(self):		#mark when leaving lot
+		num = num - 1
